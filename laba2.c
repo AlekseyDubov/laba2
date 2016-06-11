4c99918f3a1e897a2c5e73136414504eadafaf94
AlekseyDubov <freq731@yandex.ru>
2016-06-11 09:49:31 +0600

    Add printf and scanf

--- laba2.c
+++ laba2.c
@@ -3,5 +3,11 @@
 
 void main ()
 {
-
+	float a, b, c, d, x1 = 0, x2 = 0; 
+	printf("Введите a: ");
+	scanf("%f", &a);
+	printf ("Введите b: ");
+	scanf("%f", &b); 
+	printf("Введите c: ");
+	scanf("%f", &c); 
 }
